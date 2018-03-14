b5731f0f4dd91871fa242388cb10fbc28e6991c3
nickells <ellsworth.nick@gmail.com>
2018-03-13 22:54:20 -0400

    added two more examples

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const waitForTransition = (elem, propertyName) => new Promise((resolve, reject) => {
   const transitionEndListener = (evt) => {
+    console.log('got', evt, 'from', evt.target)
     if (evt.propertyName === propertyName) {
       elem.removeEventListener('transitionend', transitionEndListener)
       resolve()
@@ -8,14 +9,9 @@ const waitForTransition = (elem, propertyName) => new Promise((resolve, reject) 
   elem.addEventListener('transitionend', transitionEndListener)
 })
 
+const waitForLoad = (element) => new Promise((resolve) => element.addEventListener('load', resolve))
+
 const delay = (time) => new Promise((resolve) => setTimeout(resolve, (time)))
-
-const $bricks = [...document.getElementsByClassName('brick')]
-
-async function activateBrick($brick) {
-  $brick.classList.add('active')
-  await waitForTransition($brick, 'transform')
-}
 
 const forEachSuccessive = async (vals, func) => {
   for (let val of vals) {
@@ -24,9 +20,50 @@ const forEachSuccessive = async (vals, func) => {
   return vals
 }
 
+
+/* bricks */
+const $bricks = [...document.getElementsByClassName('brick')]
+
+async function activateBrick($brick) {
+  $brick.classList.add('active')
+  await waitForTransition($brick, 'transform')
+}
+
 async function go(){
   await delay(500)
   forEachSuccessive($bricks, activateBrick)
 }
 
-go()
+// go()
+
+/* Slideshow */
+const createSlideshow = async (container) => {
+  const images = [ ...container.children ]
+  await Promise.all(images.map(waitForLoad))
+  const playAndAdvance = async (img) => {
+    img.style.opacity = 1;
+    img.style.transition = 'opacity 500ms ease-in-out'
+    img.style.opacity = 0;
+    await waitForTransition(img, 'opacity')
+    await delay(500)
+  }
+  forEachSuccessive(images.reverse(), playAndAdvance)
+}
+// createSlideshow(document.getElementById('gallery'))
+
+/* Stagger animation */
+
+const startStagger = async (container) => {
+  await delay(50)
+  const children = [...container.children]
+  const waitAndActivate = async (child) => {
+    await delay(500)
+    child.classList.add('active')
+  }
+  await forEachSuccessive(children, waitAndActivate)
+  await delay(500)
+  const deactivate = (child) => child.classList.remove('active')
+  children.forEach(deactivate)
+}
+
+startStagger(document.getElementById('headers'))
